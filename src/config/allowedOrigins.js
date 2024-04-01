32493b5ac1f972261958938d68ef1d6df92f3550
ninthu-hologo <ninthukesan@hologo.world>
2024-04-01 16:21:04 +0530

    change the allowedOrigins

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://hologo-dash.netlify.app',
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    '*'
 ];
 
 module.exports = allowedOrigins;
