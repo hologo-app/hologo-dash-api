ca7e38e7f4e1fe2bddd94430b1c4302a0ce98f62
ninthu-hologo <ninthukesan@hologo.world>
2024-04-16 13:34:29 +0530

    setup origin

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://hologo-dash.netlify.app',
     'http://localhost:3500',
     'http://localhost:3000',
-    '*'
+    'http://54.169.193.57',
+    
 ];
 
 module.exports = allowedOrigins;
